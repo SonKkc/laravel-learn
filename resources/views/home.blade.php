5d4a21e541703bcf56e4034e9531752864120667
SonKkc <soncb.xyz@gmail.com>
2025-11-16 11:18:49 +0700

    Add home page and layout structure with navbar and footer
    - Created home.blade.php for the home page content.
    - Established default layout in default.blade.php, including responsive design and Tailwind CSS integration.
    - Added empty navbar and footer partials for future content.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <h1 class="text-4xl font-bold mb-6">Welcome to the Home Page</h1>
+    <p class="text-lg">This is the main content of the home page.</p>
+
+@endsection
